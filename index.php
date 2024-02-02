c7fa1804e01acd40ab9a2e1bacf892161a4f4cc5
Christian Hackl <hackl.chris@googlemail.com>
2024-02-02 11:53:20 +0100

    [TASK] remove unused code

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
   // Requirements / Includes
   require_once(__DIR__."/lib/gitwebhook.php");
-  
+
   // Config / Default Settings
   $configFile = __DIR__."/configs/config.json";
   if(file_exists($configFile)){
     $config = json_decode(file_get_contents($configFile),true);
-    
+
     if(!is_readable($configFile)){
       $errMsg = "[ERROR]: The Gitwebhook Config File ({$configFile}) is not accessible / readable.";
       if(ini_get('display_errors') != "1") echo "{$errMsg}";
@@ -18,16 +18,15 @@
       throw new Exception("{$errMsg}");
     }
   } else {
-    $errMsg = "[ERROR]: The Gitwebhook Config File ({$configFile}) doesn't exist (or the path is not accessible)!"; 
-    if(ini_get('display_errors') != "1") echo "{$errMsg}"; 
+    $errMsg = "[ERROR]: The Gitwebhook Config File ({$configFile}) doesn't exist (or the path is not accessible)!";
+    if(ini_get('display_errors') != "1") echo "{$errMsg}";
     throw new Exception("{$errMsg}");
   }
-  
+
   // Gitwebhook
   $webhook = new Gitwebhook($config);
-  $webhookData = $webhook->getData();
   $validate = $webhook->validateInit();
-  
+
   if($validate){
     $webhook->handle();
   }
